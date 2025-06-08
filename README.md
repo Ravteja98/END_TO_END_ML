c943f4c7e21fe6677a3acc16f1d40dc19b6aa293
Ravi Teja <ravtejchintalapudi@gmail.com>
2025-06-08 17:26:13 -0400

    Model Evaluation

--- README.md
+++ README.md
@@ -62,20 +62,20 @@ open up you local host and port
 <!-- ### dagshub
 [dagshub](https://dagshub.com/)
 
-MLFLOW_TRACKING_URI=https://dagshub.com/entbappy/End-to-end-Machine-Learning-Project-with-MLflow.mlflow \
-MLFLOW_TRACKING_USERNAME=entbappy \
-MLFLOW_TRACKING_PASSWORD=6824692c47a369aa6f9eac5b10041d5c8edbcef0 \
+MLFLOW_TRACKING_URI=https://dagshub.com/Ravteja98/END_TO_END_ML.mlflow
+	MLFLOW_TRACKING_USERNAME=Ravteja98 \
+MLFLOW_TRACKING_PASSWORD=2445c1611831684fbe298655268b6cd3fd1c6f0c \
 python script.py
 
 Run this to export as env variables:
 
 ```bash
 
-export MLFLOW_TRACKING_URI=https://dagshub.com/entbappy/End-to-end-Machine-Learning-Project-with-MLflow.mlflow
+export MLFLOW_TRACKING_URI=https://dagshub.com/Ravteja98/END_TO_END_ML.mlflow
 
-export MLFLOW_TRACKING_USERNAME=entbappy 
+export MLFLOW_TRACKING_USERNAME=Ravteja98 
 
-export MLFLOW_TRACKING_PASSWORD=6824692c47a369aa6f9eac5b10041d5c8edbcef0
+export MLFLOW_TRACKING_PASSWORD=d4efb17e7965712913e5602e4369c817bb5072e9
 
 ```
 
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from mlProject.pipeline.stage01_data_ingestion import DataIngestionTrainingPipel
 from mlProject.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage03_data_transformation import DataTransformationtrainingPipeline
 from mlProject.pipeline.stage04_model_trainer import ModelTrainerTrainingPipeline
+from mlProject.pipeline.stage05_model_evaluation import ModelEvaluationTrainingPipeline
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -41,6 +42,16 @@ STAGE_NAME =  "Model Trainer"
 try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME =  "Model Evaluation Stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelEvaluationTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
